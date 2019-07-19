5b6918018dee045bfd74b7e74e1466efab2ae6f3
Donald Benoot <dbenoot@gmail.com>
2019-07-19 18:09:46 +0200

    adding gameloop

--- engine/engine.go
+++ engine/engine.go
@@ -1,42 +1,51 @@
 package engine
 
-var turn int
+import (
+	"strconv"
 
+	gameLoop "github.com/kutase/go-gameloop"
+)
+
+var turn, day, time int
+var period string
 var moonbase Moonbase
 
-func Start() Moonbase {
+//var gl gameLoop
+
+func Start() {
 	turn = 1
+	day = 1
+	time = 1
 
 	moonbase = NewBase("Moon Base Alpha", "Research Station", "Self Funded", 10000, 5000, 300, 500, 100, 100)
 
-	return moonbase
+	gl := gameLoop.New(10, func(delta float64) {
+
+		processDateTime()
+
+	})
+
+	gl.Start()
+
 }
 
-func Input(input string) (string, int, Moonbase) {
+func Input(input string) string {
 
 	switch input {
-	case "end":
-		endTurn()
-		return input + " - Turn ended", getTurn(), moonbase
 	case "text":
-		return getText(), getTurn(), moonbase
+		return getText()
 	case "spend":
 		moonbase.Money = moonbase.Money - 1000
-		return input + " - Money spent", getTurn(), moonbase
+		return input + " - Money spent"
 	default:
-		return input + " - Unknown input", getTurn(), moonbase
+		return input + " - Unknown input"
 	}
 
-	return input + " - nothing happened", getTurn(), moonbase
+	return input + " - nothing happened"
 }
 
-func endTurn() {
-	calculateTurn()
-	turn++
-}
-
-func getTurn() int {
-	return turn
+func GetSideBarInfo() string {
+	return moonbase.Name + "\n" + moonbase.Government + "\n" + moonbase.Sponsor + "\n\n" + strconv.Itoa(moonbase.Money) + "\n" + strconv.Itoa(moonbase.Health) + "\n" + strconv.Itoa(moonbase.Lifesupport) + "\n\n" + strconv.Itoa(moonbase.Water) + "\n" + strconv.Itoa(moonbase.Food) + "\n" + strconv.Itoa(moonbase.Fuel) + "\n\nDay : " + strconv.Itoa(day) + "\nTime : " + strconv.Itoa(time)
 }
 
 func getText() string {
@@ -46,7 +55,21 @@ func getText() string {
 // func getStatus() []int {
 
 // }
+func PauseUnPause() {
 
-func calculateTurn() {
+}
 
+func processDateTime() {
+	time++
+	if time == 3600 {
+		day++
+		time = 1
+	}
+	if time < 1200 {
+		period = "Morning"
+	} else if time >= 1200 && time < 2400 {
+		period = "Afternoon"
+	} else {
+		period = "Night"
+	}
 }
--- moonbase.go
+++ moonbase.go
@@ -7,22 +7,16 @@ import (
 
 	en "github.com/dbenoot/moonbase/engine"
 	"github.com/marcusolsson/tui-go"
-	"github.com/kutase/go-gameloop"
 )
 
 var engineOutput string
 var turn int
-var base en.Moonbase
 
 func main() {
 
-
-
-	base = en.Start()
+	en.Start()
 
 	sbcontent := tui.NewLabel("SIDEBAR")
-
-	sbcontent.SetText(base.Name + "\n" + base.Government + "\n" + base.Sponsor + "\n\n" + strconv.Itoa(base.Money) + "\n" + strconv.Itoa(base.Health) + "\n" + strconv.Itoa(base.Lifesupport) + "\n\n" + strconv.Itoa(base.Water) + "\n" + strconv.Itoa(base.Food) + "\n" + strconv.Itoa(base.Fuel))
 
 	sidebar := tui.NewVBox(
 		sbcontent,
@@ -49,30 +43,21 @@ func main() {
 	command := tui.NewVBox(mainBox, inputBox)
 	command.SetSizePolicy(tui.Expanding, tui.Expanding)
 
-	gl := gameLoop.New(10, func(delta float64) {
-		input.OnSubmit(func(e *tui.Entry) {
+	input.OnSubmit(func(e *tui.Entry) {
 
-			engineOutput, turn, base = en.Input(e.Text())
+		engineOutput = en.Input(e.Text())
 
-			main.Append(tui.NewHBox(
-				tui.NewLabel("turn "+strconv.Itoa(turn)),
-				tui.NewPadder(1, 0, tui.NewLabel(" - ")),
-				tui.NewLabel(time.Now().Format("15:04")),
-				tui.NewPadder(1, 0, tui.NewLabel(" >")),
-				tui.NewLabel(engineOutput),
-				tui.NewSpacer(),
-			))
-			input.SetText("")
-
-		})
-
-		sbcontent.SetText(base.Name + "\n" + base.Government + "\n" + base.Sponsor + "\n\n" + strconv.Itoa(base.Money) + "\n" + strconv.Itoa(base.Health) + "\n" + strconv.Itoa(base.Lifesupport) + "\n\n" + strconv.Itoa(base.Water) + "\n" + strconv.Itoa(base.Food) + "\n" + strconv.Itoa(base.Fuel) + "\n\n\n" + "tick:" +  strconv.FormatFloat(delta, 'f', 6, 64	))
-
-		})
-
-	gl.Start()
-
-
+		main.Append(tui.NewHBox(
+			tui.NewLabel("turn "+strconv.Itoa(turn)),
+			tui.NewPadder(1, 0, tui.NewLabel(" - ")),
+			tui.NewLabel(time.Now().Format("15:04")),
+			tui.NewPadder(1, 0, tui.NewLabel(" >")),
+			tui.NewLabel(engineOutput),
+			tui.NewSpacer(),
+		))
+		input.SetText("")
+		sbcontent.SetText(en.GetSideBarInfo())
+	})
 
 	root := tui.NewHBox(sidebar, command)
 
@@ -82,6 +67,8 @@ func main() {
 	}
 
 	ui.SetKeybinding("Esc", func() { ui.Quit() })
+
+	ui.SetKeybinding("Space", func() { en.PauseUnPause() })
 
 	if err := ui.Run(); err != nil {
 		log.Fatal(err)
